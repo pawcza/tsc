b9fc741caca3ce54bcc5c5f80e701aaef86e36bb
pawcza <hello@pawcza.codes>
2023-04-14 17:37:16 +0200

    Webpack image fix

--- webpack.config.ts
+++ webpack.config.ts
@@ -24,6 +24,10 @@ const webpackConfig = (env): Configuration => ({
                     transpileOnly: true
                 },
                 exclude: /dist/
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource'
             }
         ]
     },
